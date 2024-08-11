863ac3c224e50dffaf47c69d73d0277dddcc4aa2
thisisimranqureshi <137511432+thisisimranqureshi@users.noreply.github.com>
2024-08-11 11:14:16 -0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.html",
+    "./public/components/*.html",
+    "./public/components/Travelling/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
